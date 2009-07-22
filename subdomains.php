14c475cdf18d281389cbb694539c7600dfa171d2
casualgenius <casualgenius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-22 08:22:09 +0000

    * License to GPL V2 * Some filename changes
    git-svn-id: http://plugins.svn.wordpress.org/wordpress-subdomains/trunk@137790 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- subdomains.php
+++ subdomains.php
@@ -6,7 +6,24 @@ Description: Setup your main categories, pages, and authors as subdomains and gi
 Version: 0.6.3
 Author: Alex Stansfield
 Author URI: http://www.casualgenius.com
+
+    Copyright 2009  Alex Stansfield  (email : alex@casualgenius.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
 
 // Include required php files
 require_once ('plugin/install.php');
